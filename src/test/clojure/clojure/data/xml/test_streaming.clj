1e51282d9f50dcc6218c258acbd1cb23b91195ce
Herwig Hochleitner <herwig@bendlas.net>
2014-10-30 00:52:39 +0100

    Test end-to-end for streaming and gc

--- src/test/clojure/clojure/data/xml/test_streaming.clj
+++ src/test/clojure/clojure/data/xml/test_streaming.clj
@@ -25,11 +25,12 @@
      end-parsed]))
 
 (defn start-large-parse [n]
-  (let [out-stream (PipedOutputStream.)
-        in-stream (PipedInputStream. out-stream 64)
+  (let [in-stream (PipedInputStream. 64)
+        out-stream (PipedOutputStream. in-stream)
         [xml-strs end-reached] (gen-large-xml n)
-        delivery (future (doseq [s xml-strs]
-                           (.write out-stream (.getBytes s "UTF-8"))))
+        delivery (future (with-open [out-stream out-stream]
+                           (doseq [s xml-strs]
+                             (.write out-stream (.getBytes s "UTF-8")))))
         xml (parse in-stream)
         result [xml delivery end-reached (WeakReference. (first (:content xml)))]]
     (is (not (realized? end-reached)) "Too eager parsing")
@@ -66,3 +67,30 @@
   which is technically impossible, because we refer to its parent a line after this")
     (is (= :el (:tag (first (:content xml)))))
     (is (= 42 (count needles)))))
+
+(defn read-full [is ba i]
+  (let [c (.read is ba i (- (alength ba) i))]
+    (if (< (+ i c) (alength ba))
+      (recur is ba (+ i c))
+      ba)))
+
+(deftest end-to-end
+  ;; Test that the star of a document can already be emitted while it
+  ;; isn't even fully parsed yet. Also while releasing head.
+  (let [n 4096
+        [xml delivery end-reached ref] (start-large-parse n)
+        in-stream (PipedInputStream. 64)
+        out-stream (PipedOutputStream. in-stream)
+        writing (future (try (with-open [out-stream out-stream]
+                               (emit xml (OutputStreamWriter. out-stream)))
+                             (catch Exception e (.printStackTrace e))))
+        bs (byte-array 125)]
+    (read-full in-stream bs 0)
+    (is (= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root><el></el><el></el><el></el><el></el><el></el><el></el><el></el><el></el><el></el>"
+           (String. bs "UTF-8")))
+    (System/gc)
+    (is (nil? (.get ref))
+        "GC didn't collect first child
+  NOTICE this might also be because of a bug in locals clearing
+         or another reason for missing effect of System/gc.")
+    (is (not (realized? end-reached)))))
